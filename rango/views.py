c44cd2e6391ad693b40924cfa12d03e9e309dc68
Jiayu77 <2495677x@student.gla.ac.uk>
2020-02-12 12:42:18 +0000

    initial commit

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
 
 def about(request):
     return render(request, 'rango/about.html')
@@ -42,8 +44,36 @@ def add_category(request):
         form = CategoryForm(request.POST)
     
         if form.is_valid():
-            cat=form.save(commit=True)
+            form.save(commit=True)
             return redirect('/rango/')
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except:
+        category = None
+
+    if category is None:
+        return redirect('/rango/')
+    
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+            else:
+                print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
